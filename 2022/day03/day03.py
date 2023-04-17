1ff9129765d0bb8655734fd522413d087204c3f0
Coit Jefferies <coitjefferies@gmail.com>
2023-04-17 16:26:17 -0400

    day03 completed

--- 2022/day03/day03.py
+++ 2022/day03/day03.py
@@ -49,3 +49,49 @@ for i in range(len(lines)):
                 tp += (upper_weights[j])
 
 print(tp)
+
+
+# Exercise 2
+print("Exercise 2:")
+
+'''
+reset tp
+divide input into groups of 3
+compare if item in first_group (fg) is also in second_group (sg) and is also in third group (tg)
+get total priority (tp)
+'''
+
+tp = 0
+
+# divide list in groups of 3
+for i in range(len(lines)):
+    while lines:
+        group = lines[0:3]
+        lines.pop(2)
+        lines.pop(1) 
+        lines.pop(0)
+    #    print(group)
+        fg = group[0]
+        sg = group[1]
+        tg = group[2]
+        print(fg)
+        print(sg)
+        print(tg)
+
+        #compare if item in first_group (fg) is also in second_group (sg) and is also in third group (tg)
+        done = []
+        for j in fg:
+            if j in sg:
+                if j in tg:
+                    if j not in done:
+                        print(tp)
+                        print("the letter " + j + " is in all 3 lists")
+                        if j in lower_weights:
+                        #print(lower_weights[j])
+                            tp += (lower_weights[j])
+                        if j in upper_weights:
+                        #print(upper_weights[j])
+                            tp += (upper_weights[j])
+                        done.append(j)
+
+print(tp)
